77723a2b15f064e184daf238bd5c2f2321085514
Rob Marano <rob@thehackerati.com>
2014-09-06 13:28:43 -0400

    Initial commit

---
+++ src/academy/thehackerati/com/Main.java
@@ -0,0 +1,9 @@
+package academy.thehackerati.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Ciao, Mondo!");
+    }
+}
