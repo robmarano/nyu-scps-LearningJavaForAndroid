c81dd8dd32b3d9a94b66748770fd6174f872f9d7
Rob Marano <rob@thehackerati.com>
2014-09-13 14:13:05 -0400

    Expanded Fibonacci example with a look-up table to save calculations and improve performance.

--- src/academy/thehackerati/com/Fibonacci.java
+++ src/academy/thehackerati/com/Fibonacci.java
@@ -1,9 +1,26 @@
 package academy.thehackerati.com;
 
+import java.util.ArrayList;
+
 /**
  * Created by rob on 9/13/14.
  */
 public class Fibonacci {
+    // members
+    protected int m_fibLength;
+    protected long m_fibValue;
+
+    // class fields
+    protected static ArrayList<Fibonacci> fibValues;
+
+    // static initializer
+    static {
+        fibValues = new ArrayList<Fibonacci>();
+    }
+
+    // class methods
+
+    // long fib(int n)
     public static long fib(int n) {
         if (n <= 1) {
             return n;
@@ -11,5 +28,36 @@ public class Fibonacci {
         else {
             return fib(n - 1) + fib(n - 2);
         }
+    }
+
+    // Fibonacci getFib(int n)
+    public static Fibonacci getFib(int n) {
+        Fibonacci returnVal = null;
+        for (Fibonacci f: fibValues) {
+            if (f.getFibLenth() == n) {
+                returnVal = f;
+            }
+        }
+        if (returnVal == null) {
+            returnVal = new Fibonacci(n);
+        }
+        return returnVal;
+    }
+
+    // ctor
+    public Fibonacci(int n) {
+        m_fibLength = n;
+        m_fibValue = Fibonacci.fib(n);
+        fibValues.add(this);
+    }
+
+    // getFibLength()
+    public int getFibLenth() {
+        return m_fibLength;
+    }
+
+    // get FibValue()
+    public long getFibValue() {
+        return m_fibValue;
     }
 }
--- src/academy/thehackerati/com/Main.java
+++ src/academy/thehackerati/com/Main.java
@@ -6,12 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        //input to print Fibonacci series for this index
-        System.out.print("Enter number to calculate Fibonacci series: ");
-        int N = new Scanner(System.in).nextInt();
-
-        for (int i = 1; i <= N; i++) {
-            System.out.println(i + ": " + Fibonacci.fib(i));
+        System.out.print("Enter number to calculate Fibonacci series, or 'q' for quit: ");
+        Scanner sc = new Scanner(System.in);
+        String nextL;
+        try {
+            while (!((nextL = sc.nextLine()).equalsIgnoreCase("q"))) {
+                System.out.println(nextL);
+                int N = 0;
+                try {
+                    N = Integer.parseInt(nextL);
+                    Fibonacci myFib = Fibonacci.getFib(N);
+                    System.out.println("Fib length = " + myFib.getFibLenth());
+                    System.out.println("Fib value = " + myFib.getFibValue());
+                } catch (IllegalArgumentException e) {
+                    System.out.println("\tError: Try again. You did not enter an integer or 'q' for quit.");
+                    //e.printStackTrace();
+                }
+                System.out.print("Enter number to calculate Fibonacci series, or 'q' for quit: ");
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
         }
     }
 }
